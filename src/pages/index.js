1b278f08cea6fa7af391634226de13a540847037
Shivashish Yadav <shivashishmca@gmail.com>
2022-11-21 06:41:00 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ const IndexPage = () => {
 						Hello there, welcome to my portfolio. I'm a passionate Project & Operation Manager Currently Working as an Operation Associate - Campaign at galleri5 based in Bangalore.
 						</p>
 						<DownloadButton
-							href="https://github.com/rashmiap/personal-website-react/raw/master/RashmiAp-Resume.pdf"
+							href="https://raw.githubusercontent.com/ShivashishY/shivanigatsbyv3/main/Shivani%20Yadav%20-%20Operation%20Associate.pdf?token=GHSAT0AAAAAAB3N7X7S7Y3Z4JJHFYX5T3AKY322ASQ"
 							download
 							title="Resume"
 						>
